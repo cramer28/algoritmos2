acce718f724213fbd4e5b62e8f05aecaa5cb0f98
Benjamín Velasco <velascobenjamin14@gmail.com>
2022-04-29 13:45:15 -0300

    Agregué los paquetes para la fecha y hora

--- practico2/tareas/src/appTareas/App.java
+++ practico2/tareas/src/appTareas/App.java
@@ -11,6 +11,8 @@ package appTareas;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class App {
     
